2ccfdec3e75075f1f5381a45576b42212331a25f
TheJD-droid <JaredDStiers@gmail.com>
2024-07-02 10:01:32 -0400

    "seTStaTe iS nOt A FUnCtioN"
    Yes it is.

--- my_website/src/components/BalloonGame/Balloon.js
+++ my_website/src/components/BalloonGame/Balloon.js
@@ -1,8 +1,10 @@
+import { Button } from '@mui/material';
 import '../../CSSFiles/balloons.css'
 import React, { useEffect, useRef } from 'react';
 
 export default function Balloon(props) {
 
+    const [balloonState, setBalloonState] = React.useState('balloon');
 
     const [chosenColor, setChosenColor] = React.useState(randomColor());
 //    const chosenColor = randomColor();
@@ -12,16 +14,60 @@ export default function Balloon(props) {
         if (props.onReset) {
             setChosenColor(randomColor())
             setChosenDuration(randomAnimationDuration());
+            setBalloonState('balloon');
+            console.log(balloonState)
+            
         }
     }, [props.onReset])
+
+    useEffect(() => {
+        console.log(balloonState);
+        console.log('is popping an instrument?')
+        if (props.toBePopped != -1) {
+            console.log('will I pop?')
+
+        }
+        console.log(props.setToBePopped)
+        if (props.toBePopped == props.idNum) {
+            console.log('I will pop!')
+            setBalloonState('balloon popped');
+            console.log(props.toBePopped)
+            console.log(balloonState)
+        }
+
+    }, [props.toBePopped])
+
+    
+
+    const handleClick = () => {
+        console.log(props);
+    };
+
     const style = {
+        
+    default: {
+        display: 'inline-block',
+        width: '60px',
+        height: '72px',
+        background: chosenColor,
+        borderRadius:'80%',
+        position:'relative',
+        boxShadow:'inset -10px -10px 0 rgba(0,0,0,0.07)',
+        margin:'20px 0px',
+        transition: 'transform 0.5s ease',
+        zIndex: 10,
+        animation: 'balloons 10s ease-in-out infinite',
+        transformOrigin: 'bottom center',
+        animationDuration: chosenDuration,
+    },
 
-        default: {
+
+        // default: {
             
-            background: chosenColor, 
-            animationDuration: chosenDuration,
+        //     background: chosenColor, 
+        //     animationDuration: chosenDuration,
 
-        },
+        // },
         balloonAfter: {
             
             //content:"▲",
@@ -43,11 +89,16 @@ export default function Balloon(props) {
     }
 
     return (
-        <div className={props.className} style={props.className === 'balloon popped' ? style.popped : style.default}>
+        <>
+        <div className={balloonState} style={balloonState === 'balloon popped' ? style.popped : style.default}>
             <div style={style.balloonAfter}>
             ▲
             </div>
         </div>
+        <Button onClick={() => {
+            handleClick();
+            }}>THIS BUTTON</Button>
+        </>
 
     );
 }
@@ -65,3 +116,4 @@ function randomAnimationDuration() {
     let duration = Math.floor(19 * Math.random()) + 1
     return (`${duration}s`);
 }
+
--- my_website/src/pages/BalloonPage.js
+++ my_website/src/pages/BalloonPage.js
@@ -5,8 +5,10 @@ import Bubble from "../components/BalloonGame/Bubble";
 import '../CSSFiles/bubble.css';
 import Balloon from "../components/BalloonGame/Balloon";
 import { Grid } from "@mui/material";
-export default function BalloonPage() {
+import { useState } from "react";
 
+
+export default function BalloonPage() {
     
       //let popSound = new Audio('../../assets/balloonpop-83760.mp3');
 
@@ -17,11 +19,19 @@ export default function BalloonPage() {
     //         popSound.play();
     //     });
     // });
-    const [balloonState, setBalloonState] = React.useState('balloon');
     const [poppingSoundNow, setPoppingSoundNow] = React.useState(false);
     const [onReset, setOnReset] = React.useState(false)
+    const [onPopped, setOnPopped] = React.useState(false)
+    const [toBePopped, setToBePopped] = useState(-1);
+    const numberOfBalloons = 10;
+/*
+    const states = new Array()
 
-
+    for (let i = 0; i < numberOfBalloons; i++) {
+        const [balloonState, setBalloonState] = React.useState('balloon');
+        states.concat({i: {'state': balloonState, 'setState': setBalloonState}})
+    }
+*/
     useEffect(() => {
         setPoppingSoundNow(false);
     }, [poppingSoundNow]);
@@ -31,44 +41,55 @@ export default function BalloonPage() {
     useEffect(() => {
         if (onReset) {
             setOnReset(false)
+            setToBePopped(-1)
         }
         resetBalloons()
     }, [onReset]);
 
-    const popBalloons = () => {
-        console.log('popBalloons')
-        setBalloonState('balloon popped')
+    useEffect(() => {
+        if(toBePopped != -1) {
+            popBalloon(toBePopped);
+            
+        }
+        // if (onPopped) {
+        //     setOnPopped(false)
+        // }
+    //}, [onPopped]);
+    }, [toBePopped]);
+
+    const popBalloon = (toBePopped) => {
+        console.log(toBePopped)
+        //setToBePopped(-1)
+        //setBalloonState('balloon popped')
       }
       
       const resetBalloons = () => {
         console.log('resetBalloons')
-        setBalloonState('balloon')
+        setToBePopped(-1)
+        
       }
+
+
+      
 
     return(
         <>
             <div className="pageContainer">
             <Grid container>
-                <Grid item>
+                {/* <Grid item>
                     <Balloon className={balloonState} onReset={onReset} setOnReset={setOnReset}></Balloon>
-                </Grid>
-                <Grid item>
-                    <Balloon className={balloonState} onReset={onReset} setOnReset={setOnReset}></Balloon>
-                </Grid>
-                <Grid item>
-                    <Balloon className={balloonState} onReset={onReset} setOnReset={setOnReset}></Balloon>
-                </Grid>
-                <Grid item>
-                    <Balloon className={balloonState} onReset={onReset} setOnReset={setOnReset}></Balloon>
-                </Grid>
-                <Grid item>
-                    <Balloon className={balloonState} onReset={onReset} setOnReset={setOnReset}></Balloon>
-                </Grid>
+                </Grid> */}
+
+                {/* {console.log(createBalloons(numberOfBalloons, balloonState, onReset, setOnReset))} */}
+                {createBalloons(numberOfBalloons, 'balloon', onReset, setOnReset, toBePopped, setToBePopped)}
+                
+                
             </Grid>
             </div>
             <div>
                 <Button onClick={() => {
-                    popBalloons()
+                    setToBePopped(5);
+                    console.log(toBePopped);
                     //setPoppingSoundNow(true)
 
                 }}>Pop</Button>
@@ -85,15 +106,53 @@ export default function BalloonPage() {
 }
 
 
-function randomColor() {
-    let firstNum = Math.floor(255 * Math.random())
-    let secondNum = Math.floor(100 * Math.random())
-    let thirdNum = Math.floor(60 * Math.random()) + 20
+// function randomColor() {
+//     let firstNum = Math.floor(255 * Math.random())
+//     let secondNum = Math.floor(100 * Math.random())
+//     let thirdNum = Math.floor(60 * Math.random()) + 20
 
-    return (`hsl(${firstNum.toString()},${secondNum.toString()}%,${thirdNum.toString()}%)`);
+//     return (`hsl(${firstNum.toString()},${secondNum.toString()}%,${thirdNum.toString()}%)`);
+// }
+
+// function randomAnimationDuration() {
+//     let duration = Math.floor(16 * Math.random()) + 4
+//     return (`${duration}s`);
+// }
+
+
+function createBalloons(numberOfBalloons, onReset, setOnReset, toBePopped, setToBePopped) {
+    // let list = new ArrayList[];
+    let result = new Array()
+    for (let i = 0; i < numberOfBalloons; i++) {
+        result = result.concat((<Grid key={`uniqueGridId${i}`}item><Balloon key={`uniqueBalloonId${i + 1}`} idNum={i + 1} onReset={onReset} setOnReset={setOnReset} toBePopped={toBePopped} setToBePopped={setToBePopped}></Balloon></Grid>));
+    }
+    return result;
 }
+/*
+function createBalloonStates(numberOfBalloons) {
+    let result = new Array()
+    for (let i = 0; i < numberOfBalloons; i++) {
+        const [balloonState, setBalloonState] = React.useState('balloon');
+        result.concat({i: {balloonState, setBalloonState}});
+    }
+}
+*/
+function trial(numberOfBalloons) {
+    let coinFlip = Math.floor(2 * Math.random())
 
-function randomAnimationDuration() {
-    let duration = Math.floor(19 * Math.random()) + 1
-    return (`${duration}s`);
+    //Heads
+    if (coinFlip === 1) {
+        return (Math.floor(numberOfBalloons * Math.random()) + 1);
+
+    }
+    //Tails
+    else if (coinFlip === 0) {
+        return 0;
+
+    }
+    //Impossible
+    else {
+        console.log('Error, coinFlip did not come up heads or tails')
+    }
+
 }
