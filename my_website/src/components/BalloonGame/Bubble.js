b2019db1ee0f058990df4730a6c468312f80ad35
TheJD-droid <JaredDStiers@gmail.com>
2024-07-02 05:25:53 -0400

    Balloons going

---
+++ my_website/src/components/BalloonGame/Bubble.js
@@ -0,0 +1,11 @@
+import '../../CSSFiles/bubble.css'
+
+export default function Bubble() {
+    return (
+        <section className="stage">
+            <figure className="ball bubble">
+            </figure>
+        </section>
+
+    );
+}
--- my_website/src/pages/BalloonPage.js
+++ my_website/src/pages/BalloonPage.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import '../CSSFiles/balloons.css';
+import { Button } from "@mui/material";
+import Bubble from "../components/BalloonGame/Bubble";
+import '../CSSFiles/bubble.css';
 
 export default function BalloonPage() {
 
-    return(
-        <>The balloons are coming</>
+    const [balloonState, setBalloonState] = React.useState('balloon');
+    const [poppingSoundNow, setPoppingSoundNow] = React.useState(false);
+    const popBalloons = () => {
+        //document.getElementById('balloon').classList.add('animation')
+        console.log('popBalloons')
+        //document.getElementsByClassName('balloon').className = document.getElementsByClassName('balloon').class + 'animation'
+        setBalloonState('balloon popped')
+      }
+      
+      const resetBalloons = () => {
+        //document.getElementById('balloon').classList.remove('animation')
+        console.log('resetBalloons')
+        setBalloonState('balloon')
+      }
 
+      //let popSound = new Audio('../../assets/balloonpop-83760.mp3');
+
+    //   document.addEventListener("DOMContentLoaded",() => {
+    //     let bubbleWrap = document.getElementById("balloon"),
+    //         popSound = new Audio('../../assets/balloonpop-83760.mp3')
+    //     bubbleWrap.addEventListener("change",() => {
+    //         popSound.play();
+    //     });
+    // });
+
+    useEffect(() => {
+        //popSound.play()
+        setPoppingSoundNow(false);
+    }, [poppingSoundNow])
+
+
+    return(
+        <>
+            <div className="pageContainer">
+                
+                <div className={balloonState}></div>
+                <div className={balloonState}></div>
+                <div className={balloonState}></div>
+                <div className={balloonState}></div>
+                <div className={balloonState}></div>
+
+            </div>
+            <div>
+                <Button onClick={() => {
+                    popBalloons()
+                    setPoppingSoundNow(true)
+
+                }}>Pop</Button>
+                <Button onClick={() => {resetBalloons()}}>Reset</Button>
+            </div>
+
+            
+        </>
     );
 }
