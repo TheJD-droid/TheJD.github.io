59cd256376b38fbe258c649f75964aae572669d2
TheJD-droid <JaredDStiers@gmail.com>
2024-10-02 19:17:51 -0400

    Need hashRouter for gh-pages

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -5,7 +5,7 @@ import { Button, ButtonGroup, Divider, useTheme } from '@mui/material';
 import Home from './pages/Home';
 import TicTacToe from './pages/TicTacToe';
 import Layout from './pages/Layout';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Winner from './pages/BalloonPage';
 import React, { useCallback, useEffect, useContext } from 'react';
@@ -15,11 +15,12 @@ import BalloonPage from './pages/BalloonPage';
 function App() {
 
 
-  const urlHome = 'https://thejd-droid.github.io/TheJDProjects/'
+  // const urlHome = 'https://thejd-droid.github.io/TheJDProjects/'
 
    return (
     <div className="App" id='root'>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter basename='TheJDProjects/'>
       <div className="App-header">
             <Navbar />
       </div>
@@ -27,17 +28,19 @@ function App() {
           
         <Routes>
             
-              <Route index element={<Home />} />
-              <Route path={urlHome} element={<TicTacToe/>}/>
-              <Route path={urlHome + 'TicTacToe'} element={<TicTacToe/>}/>
+            <Route path='TicTacToe' element={<TicTacToe/>}/>
+              {/* <Route index element={<Home />} /> */}
+              {/* <Route path={urlHome} element={<TicTacToe/>}/> */}
+              {/* <Route path={urlHome + 'TicTacToe'} element={<TicTacToe/>}/> */}
+              {/* <Route path='https://thejd-droid.github.io/TheJDProjects/' element={<Home/>} /> */}
               {/* <Route path='/JDsProjects' element={<Home />} /> */}
               {/* <Route path='/TicTacToe' element={<TicTacToe />} /> */}
               {/* <Route path='/BalloonPage' element={<BalloonPage />} /> */}
             
           </Routes>        
         </div>
-        </BrowserRouter>
-      
+        {/* </BrowserRouter> */}
+        </HashRouter>
     </div>
     
   );
